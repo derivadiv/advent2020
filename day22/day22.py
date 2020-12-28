b491e25484841c08177cfd7260e0c02488afc948
dppillai93 <dppillai93@users.noreply.github.com>
2020-12-27 20:15:20 -0600

    Part 2 Day 22
    Kicking myself for missing some of the problem spec. No fancy efficiency improvements, just better reading of the original requirements.

--- day22/day22.py
+++ day22/day22.py
@@ -49,19 +49,14 @@ def winningScore(player):
         score += (n-i)*player[i]
     return score
 
-def play(player1, player2, maxRounds = 10000):
-    rounds = 0
-    while rounds < maxRounds:
-        (player1, player2) = playRound(player1, player2)
-        rounds += 1
-        if len(player1) == 0:
-            return winningScore(player2)
-        elif len(player2) == 0:
-            return winningScore(player1)
-    return None
-
 # (player1, player2) = startingDecks('input22.txt')
 # print(play(player1, player2))
+"""
+Part 2:
+Naive implementation is too slow.
+Full input: Each player has 25 cards, so 50 total.
+  - Many combos, but not too long a key.
+"""
 
 def doRecurse(top1, player1, top2, player2):
     return len(player1) >= top1 and len(player2) >= top2
@@ -70,54 +65,34 @@ def hash4cache(player1,player2):
     return ','.join([str(x) for x in player1]) + ':' + ','.join(
         [str(x) for x in player2])
 
-cache = {}
+cache = {} # map of starting game cards to ?
 
-def playRecurseRound(player1, player2, history1, history2):
-    for i in range(len(history1)):
-        if history1[i] == player1 and history2[i] == player2:
-            # make player 1 win, not sure what happens with
-            # player 2's cards
-            # player1.extend(player2)
-            return (player1, [], history1, history2)
-    # add to game's history, otherwise
-    history1.append([x for x in player1])
-    history2.append([x for x in player2])
-
+def replayRound(player1, player2):
     # seeing if a cache helps with memory
     cachekey = hash4cache(player1, player2)
+    cachekeyreverse = hash4cache(player2, player1)
     if cachekey in cache:
-        print('cache hit, yay')
         # should be at winning level: rest of history doesn't matter
         (player1, player2) = cache[cachekey]
-        return (player1, player2, history1, history2)
-    
+        return (player1, player2)
+    elif cachekeyreverse in cache:
+        # again, winning level, skip rest of history
+        (player2, player1) = cache[cachekeyreverse]
+        return (player1, player2)
+
     top1 = player1.pop(0)
     top2 = player2.pop(0)
     if doRecurse(top1, player1, top2, player2):
-        # subgame with new history until a winner
-        player1Copy = [x for x in player1]
-        history1Copy = []
-        player2Copy = [x for x in player2]
-        history2Copy = []
-        maxRounds = 10000
-        rounds = 0
-        while rounds < maxRounds:
-            (player1Copy, player2Copy, history1Copy,
-             history2Copy) = playRecurseRound(
-                player1Copy, player2Copy, history1Copy, history2Copy)
-            if len(player1Copy) == 0:
-                # player 2 wins parent game too
-                player2.append(top2)
-                player2.append(top1)
-                cache[cachekey] = (player1, player2)
-                return (player1, player2, history1, history2)
-            elif len(player2Copy) == 0:
-                player1.append(top1)
-                player1.append(top2)
-                cache[cachekey] = (player1, player2)
-                return (player1, player2, history1, history2)
-            rounds += 1
-        print('oh no, we got to max rounds')
+        # copy that many cards in their deck for a subgame
+        (subgame1, subgame2) = replayRecurse(player1[:top1], player2[:top2])
+        if len(subgame1) > len(subgame2):
+            # player 1 won and gets this top card
+            player1.append(top1)
+            player1.append(top2)
+        else:
+            # assume player 2 won
+            player2.append(top2)
+            player2.append(top1)
     elif top1 > top2:
         # cards at bottom of own deck
         player1.append(top1)
@@ -132,21 +107,29 @@ def playRecurseRound(player1, player2, history1, history2):
         player1.append(top1)
         player2.append(top2)
     cache[cachekey] = (player1, player2)
-    return (player1, player2, history1, history2)
+    return (player1, player2)
 
-def playRecurse(player1, player2, maxRounds = 10000):
+def replayRecurse(player1, player2, maxRounds = 10000):
     rounds = 0
-    history1 = []
-    history2 = []
+    gameHistory = set()
     while rounds < maxRounds:
-        (player1, player2, history1, history2) = playRecurseRound(
-            player1, player2, history1, history2)
+        cachekey = hash4cache(player1, player2)
+        if cachekey in gameHistory:
+            # avoid history repeating itself, but
+            # what happens with player 2's cards??
+            return (player1 + player2, [])
+        (player1, player2) = replayRound(player1, player2)
         rounds += 1
-        if len(player1) == 0:
-            return winningScore(player2)
-        elif len(player2) == 0:
-            return winningScore(player1)
-    return None
+        gameHistory.add(cachekey)
+        if len(player1) == 0 or len(player2) == 0:
+            # game ends; add winningScore separately
+            return (player1, player2)
+
+def gameWinningScore(player1, player2):
+    if len(player1) == 0:
+        return winningScore(player2)
+    return winningScore(player1)
 
 (player1, player2) = startingDecks('input22.txt')
-print(playRecurse(player1, player2))
+(player1, player2) = replayRecurse(player1, player2)
+print(gameWinningScore(player1, player2))
